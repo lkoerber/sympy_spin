7f74442635e01c050d6d745bc3ae62a4e939054b
koerbe45 <l.koerber@hzdr.de>
2024-07-09 18:43:37 +0200

    Reorganize to make packaging work, child's deceases

--- sympy_spin/__init__.py
+++ sympy_spin/__init__.py
@@ -1,3 +1,13 @@
-from common import *
-from correlation_operators import *
-from spin import *
+from sympy import Basic
+from sympy.physics.quantum import *
+
+from .manipulate import order_spins, apply_commutators, order_correlators, subs_correlators
+from .common import *
+from .correlation_operators import *
+from .spin import *
+
+
+Basic.order_spins = lambda self: order_spins(self)
+Basic.apply_commutators = lambda self: apply_commutators(self)
+Basic.order_correlators = lambda self: order_correlators(self)
+Basic.subs_correlators = lambda self: subs_correlators(self)
--- sympy_spin/manipulate.py
+++ sympy_spin/manipulate.py
@@ -107,9 +107,3 @@ def order_correlators(expr):
 
     else:
         return expr.func(*(order_correlators(node) for node in expr.args))
-
-
-Basic.order_spins = lambda self: order_spins(self)
-Basic.apply_commutators = lambda self: apply_commutators(self)
-Basic.order_correlators = lambda self: order_correlators(self)
-Basic.subs_correlators = lambda self: subs_correlators(self)
