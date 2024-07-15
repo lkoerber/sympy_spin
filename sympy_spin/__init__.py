c27ad3470cb66ddd4174b1b02eb0e3d0f7e54a45
koerbe45 <l.koerber@hzdr.de>
2024-07-15 16:39:29 +0200

    Fix imports

--- sympy_spin/__init__.py
+++ sympy_spin/__init__.py
@@ -5,7 +5,7 @@ from .manipulate import order_spins, apply_commutators, order_correlators, subs_
 from .common import *
 from .correlation_operators import *
 from .spin import *
-
+from .boson import *
 
 Basic.order_spins = lambda self: order_spins(self)
 Basic.apply_commutators = lambda self: apply_commutators(self)
